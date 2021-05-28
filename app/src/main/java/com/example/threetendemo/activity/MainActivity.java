f3706c70e5a5b347712e86c47d10dd4d411a7e88
Manali Modi <modimanali231@gmail.com>
2021-05-28 19:03:02 +0530

    Added more conversations

--- app/src/main/java/com/example/threetendemo/activity/MainActivity.java
+++ app/src/main/java/com/example/threetendemo/activity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.threetendemo.activity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.threetendemo.R;
 import com.example.threetendemo.databinding.ActivityMainBinding;
 
 import org.threeten.bp.Clock;
 import org.threeten.bp.DateTimeUtils;
+import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.Duration;
 import org.threeten.bp.Instant;
 import org.threeten.bp.LocalDate;
@@ -23,6 +24,7 @@ import org.threeten.bp.ZoneOffset;
 import org.threeten.bp.ZonedDateTime;
 import org.threeten.bp.chrono.Chronology;
 import org.threeten.bp.format.DateTimeFormatter;
+import org.threeten.bp.temporal.TemporalAccessor;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -32,6 +34,7 @@ import androidx.databinding.DataBindingUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "DateTime";
     private ActivityMainBinding mainBinding;
 
     @Override
@@ -41,74 +44,176 @@ public class MainActivity extends AppCompatActivity {
 
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        //showDateTime();
-        initButtonClick();
+        basicClasses();
+        convertUTCToZone();
+        convertZoneToUTC(); //has internal conversion Zone->Local & Local->Zone
+        convertStringToLocalOrZone();
+        convertLocalToString();
+        getSeparateFields();
+        inputDateWithZoneId();
+        inputDateWithZoneOffset();
+        inputDateTimeWithFractionOrNot();
+        inputDateTimeOtherFormats(); //Not Standard
+        inputDateInMillis();
     }
 
-    @SuppressLint("SetTextI18n")
-    private void initButtonClick() {
-        mainBinding.btnCurrentUTC.setOnClickListener(view -> mainBinding.txtDateTime.setText(Instant.now().toString()));
+    private void inputDateInMillis() {
+        String TAG = "InMillis";
+        long millis = 1569494827;
 
-        mainBinding.btnCurrentZone.setOnClickListener(view -> mainBinding.txtDateTime.setText(ZonedDateTime.now().toString()));
-
-        mainBinding.btnLocal.setOnClickListener(view -> mainBinding.txtDateTime.setText(LocalDateTime.now().toString()));
-
-        mainBinding.btnUTCToZone.setOnClickListener(view -> {
-            Instant instant = Instant.now();
-            ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneId.of("Asia/Kolkata"));
-            mainBinding.txtDateTime.setText("From UTC:\n" + instant + "\n\nTo Zone:\n" + zdt);
-        });
-
-        mainBinding.btnZoneToUTC.setOnClickListener(view -> {
-            ZonedDateTime zdt = ZonedDateTime.now();
-            LocalDateTime ldt1 = LocalDateTime.ofInstant(zdt.toInstant(), ZoneOffset.systemDefault());
-            ZonedDateTime zdt1 = ldt1.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneOffset.UTC);
-            mainBinding.txtDateTime.setText("From Zone:\n" + zdt + "\n\nTo UTC:\n" + zdt1);
-        });
-
-        mainBinding.btnStringToLocal.setOnClickListener(view -> {
-            String strDate = "27/05/2021 03:30 PM";
-            LocalDateTime ldt = LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a"));
-            mainBinding.txtDateTime.setText("From String:\n" + strDate + "\n\nTo Local:\n" + ldt);
-        });
-
-        mainBinding.btnLocalToString.setOnClickListener(view -> {
-            LocalDateTime ldt = LocalDateTime.now();
-            String dateInString = ldt.format(DateTimeFormatter.ofPattern("dd MMM, yyyy HH:mm:ss"));
-            mainBinding.txtDateTime.setText("From Local:\n" + ldt + "\n\nTo String:\n" + dateInString);
-        });
-
-        mainBinding.btnDiffFormats.setOnClickListener(view -> {
-            String input = "2021-05-27T16:05:15.598";
-            LocalDateTime ldt = LocalDateTime.parse(input); //No need to use formatter because String input is already in format of LocalDateTime
-            String output = ldt.format(DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a"));
-            mainBinding.txtDateTime.setText("From:\n" + input + "\n\nTo:\n" + output);
-
-            int year = ldt.getYear();
-            Month month = ldt.getMonth();
-            int day = ldt.getDayOfMonth();
-            int hour = ldt.getHour();
-            int min = ldt.getMinute();
-            int sec = ldt.getSecond();
-            int h;
-            String ampm;
-            if(hour < 12){
-                h = hour;
-                ampm = "AM";
-            }
-            else {
-                if(hour == 12) h = hour;
-                else h = hour - 12;
-                ampm = "PM";
-            }
-            Chronology chronology = ldt.getChronology();
-            mainBinding.txtDateTime.append("\n\nYear: " + year + " Month: " + month + " Day: " + day);
-            mainBinding.txtDateTime.append("\nHour: " + h + " Minutes: " + min + " Seconds: " + sec + " am/pm: " + ampm);
-            mainBinding.txtDateTime.append("\nChronology: " + chronology);
-        });
+        Instant instant = Instant.ofEpochMilli(millis);
+        LocalDateTime ldt = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
+        Log.d(TAG, "To instant: " + instant);
+        Log.d(TAG, "To local: " + ldt);
+        Log.d(TAG, "To zone: " + zdt);
     }
 
-    private void showDateTime() {
+    private void inputDateTimeOtherFormats() {
+        String TAG = "OtherFormats";
+        String date1 = "2020-03-22 05:06:07.000 AM";//yyyy-MM-dd hh:mm:ss.SSS a
+        String date2 = "2020-03-22+05:30";//yyyy-MM-ddXXX
+        String date3 = "2020-03-22 AD";//yyyy-MM-dd G
+        String date4 = "2020-03-22";//yyyy-MM-dd
+        String date5 = "22 Mar, 2021";//dd EEE, yyyy
+        String date6 = "05:06:07.089";//HH:mm:ss.SSS
+        //Or many more possible here
+
+        //take format of either date1, date2, date3, date4, date5 or date6 as per input
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.SSS a");
+
+        //Directly convert to zone not possible if don't have any info of zone. So, first convert it into local and then to zone as per convertZoneToUTC()
+        LocalDateTime ldt1 = LocalDateTime.parse(date1, formatter1);
+
+        String output1 = ldt1.format(DateTimeFormatter.ofPattern("EEE dd MMM yyyy"));//Sun 22 Mar 2020
+        String output2 = ldt1.format(DateTimeFormatter.ofPattern("D Q YYYY"));//DayOfYear-Quarter-WeekBasedYear -> 82 1 2020
+        Log.d(TAG, "\n" + date1 + "-->" + output1);
+        Log.d(TAG, "\n" + date1 + "-->" + output2);
+    }
+
+    private void inputDateTimeWithFractionOrNot() {
+        String TAG = "DateTimeFormats";
+        String date1 = "2021-05-28T07:16:12.526";//yyyy-MM-dd'T'HH:mm:ss.SSS
+        String date2 = "2021-05-28T07:16:12.526Z";//yyyy-MM-dd'T'HH:mm:ss.SSS'Z'
+        String date3 = "2021-05-28T07:16:12";//yyyy-MM-dd'T'HH:mm:ss
+
+        //take format of either date1, date2, date3 or date4 as per input
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+        //To convert into zone, For date 1 or date 3 refer convertZoneToUTC()
+        //And for date2 refer convertUTCToZone()
+        LocalDateTime ldt1 = LocalDateTime.parse(date1, formatter1);
+
+        //take any other output format or as much as want
+        String output1 = ldt1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));//2020-03-22
+        Log.d(TAG, "\n" + date1 + "-->" + output1);
+    }
+
+    private void inputDateWithZoneOffset() {
+        String TAG = "DateWithZoneOffset";
+        String date1 = "2020-03-22T05:06:07.000+05:30";//yyyy-MM-dd'T'HH:mm:ss.SSSXXX
+        String date2 = "2020-03-22T05:06:07.000+0530";//yyyy-MM-dd'T'HH:mm:ss.SSSXX
+        String date3 = "2020-03-22T05:06:07+05:30";//yyyy-MM-dd'T'HH:mm:ssXXX
+        String date4 = "2020-03-22T05:06:07+0530";//yyyy-MM-dd'T'HH:mm:ssXX
+
+        //take format of either date1, date2, date3 or date4 as per input
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+        //take either local or zone as per requirement
+        LocalDateTime ldt1 = LocalDateTime.parse(date1, formatter1);
+        ZonedDateTime zdt = ZonedDateTime.parse(date1, formatter1);
+
+        //take any other output format or as much as want
+        String output1 = ldt1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd G"));//2020-03-22 AD
+        String output2 = zdt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'['VV']'"));//2020-03-22T05:06:07.000[+05:30]
+        String output3 = zdt.format(DateTimeFormatter.ofPattern("dd MMMM yyyy z"));//22 March 2020 +05:30
+        Log.d(TAG, "\n" + date1 + "-->" + output1);
+        Log.d(TAG, "\n" + date1 + "-->" + output2);
+        Log.d(TAG, "\n" + date1 + "-->" + output3);
+    }
+
+    private void inputDateWithZoneId() {
+        String TAG = "DateWithZoneId";
+        String date1 = "2020-03-22T05:06:07.000[Asia/Kolkata]"; //yyyy-MM-dd'T'HH:mm:ss.SSS'['VV']'
+        String date2 = "2020-03-22T05:06:07.000+05:30[Asia/Kolkata]"; //yyyy-MM-dd'T'HH:mm:ss.SSSXXX'['VV']'
+        String date3 = "2020-03-22T05:06:07+05:30[Asia/Kolkata]"; //yyyy-MM-dd'T'HH:mm:ssXXX'['VV']'
+
+        //take format of either date1, date2 or date3 as per input
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'['VV']'");
+
+        //take either local or zone as per requirement
+        LocalDateTime ldt1 = LocalDateTime.parse(date1, formatter1);
+        ZonedDateTime zdt = ZonedDateTime.parse(date1, formatter1);
+
+        //take any other output format or as much as want
+        String output1 = ldt1.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss.SSS a"));//2020-03-22 05:06:07.000 AM
+        String output2 = zdt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXX"));//2020-03-22T05:06:07+0530
+        String output3 = zdt.format(DateTimeFormatter.ofPattern("dd MMMM yyyy z"));//22 March 2020 IST
+        Log.d(TAG, "\n" + date1 + "-->" + output1);
+        Log.d(TAG, "\n" + date1 + "-->" + output2);
+        Log.d(TAG, "\n" + date1 + "-->" + output3);
+    }
+
+    private void getSeparateFields() {
+        String input = "2021-05-27T16:05:15.598+05:30[Asia/Kolkata]";
+
+        //Also possible using local
+        ZonedDateTime zdt = ZonedDateTime.parse(input);
+
+        int year = zdt.getYear();
+        Month month = zdt.getMonth();
+        int day = zdt.getDayOfMonth();
+        int hour = zdt.getHour();
+        int min = zdt.getMinute();
+        int sec = zdt.getSecond();
+        DayOfWeek dow = zdt.getDayOfWeek();
+        int doy = zdt.getDayOfYear();
+        ZoneId z = zdt.getZone();
+        ZoneOffset zo = zdt.getOffset();
+        Chronology ch = zdt.getChronology();
+        Log.d(TAG, "Year: " + year);
+        Log.d(TAG, "Month: " + month.toString());
+        Log.d(TAG, "Day: " + day);
+        Log.d(TAG, "Hour: " + hour);
+        Log.d(TAG, "Minutes: " + min);
+        Log.d(TAG, "Seconds: " + sec);
+        Log.d(TAG, "DayOfWeek: " + dow);
+        Log.d(TAG, "DayOfYear: " + doy);
+        Log.d(TAG, "ZoneId: " + z);
+        Log.d(TAG, "ZoneOffset: " + zo);
+        Log.d(TAG, "Chronology: " + ch);
+    }
+
+    private void convertLocalToString() {
+        LocalDateTime ldt = LocalDateTime.now();//2021-05-28T12:46:12.533
+        String dateInString = ldt.format(DateTimeFormatter.ofPattern("dd MMM, yyyy HH:mm:ss"));//28 May, 2021 12:46:12
+        Log.d(TAG, "LocalToString: " + ldt + "-->" + dateInString);
+    }
+
+    private void convertStringToLocalOrZone() {
+        String strDate = "27/05/2021 03:30 PM";
+        LocalDateTime ldt = LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a"));//2021-05-27T15:30
+        Log.d(TAG, "StringToLocal: " + strDate + "-->" + ldt);
+    }
+
+    private void convertZoneToUTC() {
+        ZonedDateTime zdt = ZonedDateTime.now();//2021-05-28T11:44:59.596+05:30[Asia/Kolkata]
+        LocalDateTime ldt = zdt.toLocalDateTime();//2021-05-28T07:16:12.527 //this and below both are same
+        LocalDateTime ldt1 = LocalDateTime.ofInstant(zdt.toInstant(), ZoneOffset.systemDefault());//2021-05-28T07:16:12.527
+        ZonedDateTime zdt1 = ldt.atZone(ZoneId.systemDefault()).withZoneSameInstant(ZoneOffset.UTC);//2021-05-28T07:16:12.527Z
+        Log.d(TAG, "ZoneToUTC: " + zdt + "-->" + zdt1);
+        Log.d(TAG, "ZoneToLocal: " + zdt + "-->" + ldt);
+        Log.d(TAG, "LocalToZone: " + ldt + "-->" + zdt1);
+    }
+
+    private void convertUTCToZone() {
+        Instant instant = Instant.now(); //2021-05-28T07:16:12.526Z
+        ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneId.of("Asia/Kolkata"));//2021-05-28T12:46:12.526+05:30[Asia/Kolkata]
+        Log.d(TAG, "UTCToZone: " + instant + "-->" + zdt);
+    }
+
+    private void basicClasses() {
         Instant i1 = Instant.now(); //shows current utc time
         mainBinding.txtDateTime.append("Instant 1 - " + i1);
 
@@ -121,11 +226,11 @@ public class MainActivity extends AppCompatActivity {
         Clock c3 = Clock.systemDefaultZone();
         mainBinding.txtDateTime.append("\nClock 3 - " + c3);
 
-        /*Clock c4 = Clock.fixed(Instant.now(), ZoneId.of("Europe/Paris"));
-        mainBinding.txtDateTime.append( "\nClock 4 - " + c4);
+        Clock c4 = Clock.fixed(Instant.now(), ZoneId.of("Europe/Paris"));
+        mainBinding.txtDateTime.append("\nClock 4 - " + c4);
 
         Clock c5 = Clock.offset(Clock.system(ZoneId.of("Europe/Paris")), Duration.ofSeconds(2));
-        mainBinding.txtDateTime.append( "\nClock 5 - " + c5);*/
+        mainBinding.txtDateTime.append("\nClock 5 - " + c5);
 
         Instant start = Instant.now();
         Instant end1 = Instant.now().plusSeconds(5);
